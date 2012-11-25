304b135f82f8aadea2a3e3c5f011775a51c2c030
ariteku <ariteku@gmail.com>
2012-11-25 14:57:28 +0900

    fix

--- initApplication.sh
+++ initApplication.sh
@@ -8,6 +8,7 @@ curl -s https://getcomposer.org/installer | php
 php composer.phar install
 
 # create sqlite3 db
-cat ./db/build.sql | sqlite3 ./db/app.db
+sqlite3 ./db/app.db < ./db/build.sql
+
 chown -R apache:apache ./db
 
